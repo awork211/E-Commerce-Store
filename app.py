d81c460f31b792f2502e671c8f436129d6e4915a
awork211 <work.aaron89@gmail.com>
2023-01-25 14:58:47 -0500

    templates, route init

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request, url_for, redirect, session
 
 app = Flask(__name__)
 
+# add db connection
+
+# make db models
+
+# start adding login to db
+
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
